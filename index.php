092d2e5cb5d95110acdfe1ffbb25be4ad4303c00
Mohamed-Boutarbouch <Mohamed-Boutarbouch>
2023-11-16 16:26:42 +0100

    Render notes and note

--- index.php
+++ index.php
@@ -2,14 +2,4 @@
 
 require 'functions.php';
 require 'Database.php';
-// require 'router.php';
-
-$config = require('config.php');
-
-$db = new Database($config['database']);
-
-$id = $_GET['id'];
-$query = "SELECT * FROM posts where id = ?";
-
-
-dd($db->query($query, [$id])->fetch(PDO::FETCH_ASSOC));
+require 'router.php';
